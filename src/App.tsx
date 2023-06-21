d508990fed9735b6555ded17f1759703a760fcfe
SUYEON KIM <yeonee230@gmail.com>
2023-06-21 20:02:30 +0900

    배열

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,23 @@ import * as D from './data'
 
 function App() {
   return (
+    
     <div className="App">
-     <p>
-      {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
-     </p>
-     <img src={D.randomAvatar()} height="50" />
-     <img src={D.randomImage()} height="300" />
+    {D.makeArray(10).map((notUsed, index) => (
+      <div key={index}>
+        <p>{D.randomId()}</p>
+        <p>{D.randomName()}</p>
+        <p>{D.randomJobTitle()}</p>
+        <p>{D.randomSentence()}</p>
+        <img src={D.randomAvatar()} width={100} height={100} alt='Avatar'/>
+        <hr />
+      </div>
+      ))}
+    {/* //  <p>
+    //   {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+    //  </p>
+    //  <img src={D.randomAvatar()} height="50" alt='small img' />
+    //  <img src={D.randomImage()} height="300" alt='Large img'/> */}
 
     </div>
   );
